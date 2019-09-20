a1c21e95215b3b65cfb17c70c372f8f543911fc1
xu <xu@xu.com>
2019-09-20 16:42:40 +0800

    third commit

--- src/com/xiao/HellGit.java
+++ src/com/xiao/HellGit.java
@@ -4,5 +4,6 @@ public class HellGit {
     public static void main(String[] args) {
         System.out.println("hello,git!");
         System.out.println("change!!");
+        System.out.println("change more!");
     }
 }
